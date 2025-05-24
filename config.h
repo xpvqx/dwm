669c2804c108639c2efdc007d22638613d0fbecd
averyiaa <aaveryia@gmail.com>
2025-05-25 01:58:56 +0200

    fix path

--- config.h
+++ config.h
@@ -88,7 +88,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *flamecmd[] = { "flameshot", "gui", NULL };
-static const char *pdfscript[] = { "/home/karl/git/scripts/readPdf.sh", NULL };
+static const char *pdfscript[] = { "/home/karl/git/scripts/readpdf.sh", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
