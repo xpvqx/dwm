97be6c656903e808cddc0e37ba6ea6d68676e711
xpvqx <kjs02x@gmail.com>
2025-07-22 01:45:19 +0200

    colors

--- config.h
+++ config.h
@@ -23,8 +23,10 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#ffffff";	/* highlight color inside */
-static const char col_cyan[]        = "#8dbf7c";	/* brown color outside  */
-// static const char col_cyan[]        = "#707780";	/* teal color outside  */
+static const char col_cyan[]        = "#8dbf7c";	/* highlight color outside  */
+
+/* brown: #8dbf7c */
+/* teal: #707780 */
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
